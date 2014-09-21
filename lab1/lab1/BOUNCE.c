9810c7b6ca3948267c55fd5d13f7ee45acf73078
Sevsoad <sevsoad@ya.ru>
2014-09-21 20:54:33 +0300

    Now coach jumps

--- lab1/lab1/BOUNCE.c
+++ lab1/lab1/BOUNCE.c
@@ -1,16 +1,13 @@
 #include "OpenGLSB.h"	
 #include <stdio.h>
 #include <math.h>
-#define PI 3.14159265358979323846
 
-
-// Keep track of windows changing width and height
 GLfloat windowWidth;
 GLfloat windowHeight;
 
 // Object coordinates on screen
-GLfloat xBackground = 150.0f;
-GLfloat yBackground = -30.0f;
+GLfloat xTrampoline = 150.0f;
+GLfloat yTrampoline = -30.0f;
 
 GLfloat xCoach = -150.0f;
 GLfloat yCoach = 1.0f;
@@ -19,16 +16,20 @@ GLfloat wheelRadius = 8.0f;
 int spokesRotationDelay = 20;
 int spokesRotationTime = 0;
 
+//fly scene
+GLfloat flightLength = -40;
 
 //Size of a step (speed)
 GLfloat xBackgrStep = 2.0f;
-GLfloat yBackgrStep = 2.0f;
+GLfloat yBackgrStep = 0.0f;
+GLfloat xCoachStep = 1.0f;
 
-GLfloat xCoachStep = 2.0f;
+//other vars
 float x2,y2 = 0.0f; //tmp values to draw circle
- 
-int j = 0;
 
+int j = 0;
+GLfloat angle = 0;
+#define PI 3.14159265
 
 void DrawCircle(int xPos, int yPos) {
 	glColor3ub(139, 119, 101);
@@ -74,21 +75,28 @@ void DrawSpokes(GLfloat xPos, GLfloat yPos)
 	glEnd();	
 }
 
-void DrawTrampoline(int xWheel, int yWheel)
+void DrawTrampoline()
 {
 	glColor3ub(135, 135, 135);
 	glBegin(GL_TRIANGLES);
-	glVertex3f(-50.0 + xWheel, 0.0 + yWheel, 0.0);
-	glVertex3f(0.0 + xWheel, 30.0 + yWheel, 0.0);
-	glVertex3f(0.0 + xWheel, 0.0 + yWheel, 0.0);
+	glVertex2f(xTrampoline - 50.f, yTrampoline);
+	glVertex2f(xTrampoline, yTrampoline + 30.f);
+	glVertex2f(xTrampoline, yTrampoline);
 	glEnd();
 }
 
 void DrawCoach()
 {
+	glPushMatrix();
+	if (angle <= 36 && angle > 0){
+		glTranslatef(0, angle - 1, 0);
+	}
+	
+	glRotatef(angle, 0, 0, 1);
+
 	//coach body
 	glColor3ub(77, 77, 77);
-	glRectf(xCoach + 10, yCoach, xCoach +  30, yCoach - 28);
+	glRectf(xCoach + 10, yCoach, xCoach + 30, yCoach - 28);
 	glRectf(xCoach - 5, yCoach, xCoach + 10, yCoach - 18);
 	glRectf(xCoach, yCoach, xCoach + 45, yCoach - 18);
 
@@ -97,6 +105,7 @@ void DrawCoach()
 	glVertex3f(xCoach - 9, yCoach, 0.0);
 	glVertex3f(xCoach - 5, yCoach - 18, 0.0);
 	glEnd();
+
 
 	glBegin(GL_TRIANGLES);
 	glVertex3f(xCoach + 45, yCoach, 0.0);
@@ -129,53 +138,69 @@ void DrawCoach()
 	DrawCircle(xCoach + 45, yCoach - 23); //right
 
 	//Rotating spokes
-	DrawSpokes(xCoach - 5, yCoach - 23);
-	DrawSpokes(xCoach + 45, yCoach - 23);
+	DrawSpokes(xCoach - 5, yCoach - 23); //left
+	DrawSpokes(xCoach + 45, yCoach - 23); //right
+	
+	glPopMatrix();
 }
 
-
-///////////////////////////////////////////////////////////
-// Called to draw scene
 void RenderScene(void)
-	{
-	// Clear the window with current clearing color
+{
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glColor3ub(0, 100, 0);
-	glRectf(-400, -30, 400, -300);
+	glRectf(-400, -30, 400, -300); //background grass
 
-	DrawTrampoline(xBackground, yBackground);
+	DrawTrampoline();	
+	DrawCoach(0, 0);	
 
-	DrawCoach(0, 0);
+	glutSwapBuffers();
+}
 
-    // Flush drawing commands and swap
-    glutSwapBuffers();
-	}
-
-///////////////////////////////////////////////////////////
-// Called by GLUT library when idle (window not being
-// resized or moved)
 void TimerFunction(int value)
 {
-	xBackground -= xBackgrStep;
-	xCoach += xCoachStep;
+	xTrampoline -= xBackgrStep;
+
+	if (xCoach + 95 >= xTrampoline && xCoach < xTrampoline + 120 && xCoach < flightLength - 10)
+	{
+		yCoach += xCoachStep;
+
+		if (angle < 36)
+			angle += 3;
+
+		xCoach += xCoachStep* 0.8;
+	}
+	else
+	{
+		if (xCoach >= flightLength - 10 && xCoach < flightLength + 7)
+		{
+			xCoach += xCoachStep * 0.8;
+		} else if (xCoach >= flightLength + 7)
+		{
+			if (xCoach >= flightLength + 10)
+			{
+				yCoach -= xCoachStep * 1;
+				xCoach += xCoachStep * 0.2;
+			}				
+			else{
+				yCoach -= xCoachStep * 0.4;
+				xCoach += xCoachStep * 0.5;
+			}				
+			
+		} else{
+			xCoach += xCoachStep;
+		}		
+	}
 
 	glutPostRedisplay();
 	glutTimerFunc(25, TimerFunction, 1);
 }
 
-
-///////////////////////////////////////////////////////////
-// Setup the rendering state
 void SetupRC(void)
-    {
-    // Set clear color to blue
-    glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
-    }
+{
+	glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
+}
 
-
-///////////////////////////////////////////////////////////
-// Called by GLUT library when the window has chanaged size
 void ChangeSize(int w, int h)
     {
     GLfloat aspectRatio;
@@ -210,8 +235,6 @@ void ChangeSize(int w, int h)
     glLoadIdentity();
     }
 
-///////////////////////////////////////////////////////////
-// Main program entry point
 int main(int argc, char* argv[])
 	{
 	glutInit(&argc, argv);
