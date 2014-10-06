c84852271e02fca15a09fe3315c27aca1b545419
Sevsoad <sevsoad@ya.ru>
2014-10-07 02:58:10 +0300

    Wheels and rotation added. Coach rides.

--- lab2/3DCoach/3DCoach/Main.cpp
+++ lab2/3DCoach/3DCoach/Main.cpp
@@ -18,18 +18,23 @@ float x = 0.0f, z = 5.0f;
 
 float deltaAngle = 0.0f;
 float coachPos = 0.0f;
-float coachSpan = 40.0f;
+float coachSpan = 50.0f;
 float deltaMove = 0;
+float rotateAngle = 0;
+float rotationCount = 0;
+float rangeFromPlayer = 10;
 std::vector< glm::vec3 > vertices;
+std::vector< glm::vec3 > wheel1;
+std::vector< glm::vec3 > wheel2;
 
-std::vector< glm::vec3 > loadObj(std::vector< glm::vec3 > vertices)
+std::vector< glm::vec3 > loadObj(string path)
 {
 	std::vector< unsigned int > vertexIndices;//, uvIndices, normalIndices;
 	std::vector< glm::vec3 > temp_vertices;
 	std::vector< glm::vec3 > result_vertices;
 	string word;
 
-	ifstream myfile("D:\\coach5.obj");
+	ifstream myfile(path);
 	if (myfile.is_open())
 	{
 		while (myfile >> word)
@@ -86,13 +91,73 @@ void computeDir(float deltaAngle) {
 void DrawCoach() {
 	glPushMatrix();
 	glColor3f(1.0f, 0.5f, 0.3f);
-	glTranslatef(coachSpan - coachPos, 0.8f, -13.5f);
+	glTranslatef(coachSpan - coachPos, 0.85f, -13.5f + rangeFromPlayer);
 
 	glBegin(GL_TRIANGLES);	
 	for (int i = 0; i < vertices.size(); i++){
 		glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
 	}
 	glEnd();
+	glPopMatrix();	
+}
+
+void DrawWheels()
+{
+	glPushMatrix();
+		glTranslatef(coachSpan - coachPos, 0, rangeFromPlayer);
+
+		//big wheel 1
+		glPushMatrix();		
+			glTranslatef(2.0, 0.89f, -12.55f);
+			glPushMatrix(); 
+				glRotatef(rotateAngle, 0, 0, 1);
+				glBegin(GL_TRIANGLES);
+					for (int i = 0; i < wheel1.size(); i++){
+						glVertex3f(wheel1[i].x, wheel1[i].y, wheel1[i].z);
+					}
+				glEnd();
+			glPopMatrix();		
+		glPopMatrix();
+
+		//big wheel2
+		glPushMatrix();		
+		glTranslatef(2.0, 0.89f, -14.5f);
+			glPushMatrix(); 
+				glRotatef(rotateAngle, 0, 0, 1);
+				glBegin(GL_TRIANGLES);
+					for (int i = 0; i < wheel1.size(); i++){
+						glVertex3f(wheel1[i].x, wheel1[i].y, wheel1[i].z);
+					}
+				glEnd();
+			glPopMatrix();		
+		glPopMatrix();
+
+		//small wheel 1
+		glPushMatrix();		
+			glTranslatef(-1.5f, 0.65f, -14.6f);
+			glPushMatrix(); 
+				glRotatef(rotateAngle, 0, 0, 1);
+				glBegin(GL_TRIANGLES);
+					for (int i = 0; i < wheel2.size(); i++){
+						glVertex3f(wheel2[i].x, wheel2[i].y, wheel2[i].z);
+					}
+				glEnd();
+			glPopMatrix();		
+		glPopMatrix();
+
+		//small wheel 2
+		glPushMatrix();		
+			glTranslatef(-1.5f, 0.65f, -12.5f);
+			glPushMatrix(); 
+				glRotatef(rotateAngle, 0, 0, 1);
+				glBegin(GL_TRIANGLES);
+					for (int i = 0; i < wheel2.size(); i++){
+						glVertex3f(wheel2[i].x, wheel2[i].y, wheel2[i].z);
+					}
+				glEnd();
+			glPopMatrix();		
+		glPopMatrix();
+
 	glPopMatrix();
 }
 
@@ -109,7 +174,8 @@ void RenderScene(void)
 		x + lx, 1.0f, z + lz,
 		0.0f, 1.0f, 0.0f);
 
-	glColor3f(0.9f, 0.3f, 0.9f); //background
+	GLfloat color[] = { 0.384f, 0.776f, 0.533f, 1.f };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color);
 	glBegin(GL_QUADS);
 	glVertex3f(-100.0f, 0.0f, -100.0f);
 	glVertex3f(-100.0f, 0.0f, 100.0f);
@@ -117,19 +183,28 @@ void RenderScene(void)
 	glVertex3f(100.0f, 0.0f, -100.0f);
 	glEnd();
 
+	GLfloat color2[] = { 1.f, 0.8f, 1.f, 0.2f };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color2);
 	DrawCoach();
+
+	GLfloat color3[] = { 0.862, 0.803, 0.156, 0.2f };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color3);
+	DrawWheels();
+
 	glutSwapBuffers();
 }
 
 void TimerFunc(int value)
 {
-	if (coachPos < 80){
-		coachPos += 0.2f;
+	if (coachPos < coachSpan + 40){
+		coachPos += 0.2f;		
 	}
 	else {
 		coachPos = 0;
 	}
-	
+
+	rotateAngle+=5;
+
 	glutPostRedisplay();
 	glutTimerFunc(14, TimerFunc, 1);
 }
@@ -159,10 +234,10 @@ void changeSize(int w, int h) {
 void PressKey(int key, int xx, int yy) {
 
 	switch (key) {
-	case GLUT_KEY_LEFT: deltaAngle = -0.007f; break;
-	case GLUT_KEY_RIGHT: deltaAngle = 0.007f; break;
-	case GLUT_KEY_UP: deltaMove = 0.3f; break;
-	case GLUT_KEY_DOWN: deltaMove = -0.3f; break;
+	case GLUT_KEY_LEFT: deltaAngle = -0.02f; break;
+	case GLUT_KEY_RIGHT: deltaAngle = 0.02f; break;
+	case GLUT_KEY_UP: deltaMove = 0.9f; break;
+	case GLUT_KEY_DOWN: deltaMove = -0.9f; break;
 	}
 }
 
@@ -192,9 +267,10 @@ int main(int argc, char* argv[])
 	glutDisplayFunc(RenderScene);
 	glutIdleFunc(RenderScene);
 	glutTimerFunc(14, TimerFunc, 1);
-	//std::vector< glm::vec2 > uvs;
 
-	vertices = loadObj(vertices);
+	vertices = loadObj("D:\\coach5.obj");
+	wheel1 = loadObj("D:\\wheel4.obj");
+	wheel2 = loadObj("D:\\wheel3.obj");
 
 	glutSpecialFunc(PressKey);
 	glutSpecialUpFunc(ReleaseKey);
@@ -204,7 +280,7 @@ int main(int argc, char* argv[])
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-	GLfloat lightpos[] = { 0.0, 5.0, -5.5, 0.7};
+	GLfloat lightpos[] = { 0.0, 0.0, 0.0, 0.4};
 	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
 
 	glutMainLoop();
