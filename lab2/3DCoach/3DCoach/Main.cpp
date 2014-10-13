6f61fd1dffe76daf2cea765d6e07f96e194dc940
Sevsoad <sevsoad@ya.ru>
2014-10-13 22:17:59 +0300

    Proper lighting added

--- lab2/3DCoach/3DCoach/Main.cpp
+++ lab2/3DCoach/3DCoach/Main.cpp
@@ -9,11 +9,8 @@
 
 using namespace std;
 
-// angle of rotation for the camera direction
 float angle = 0.0f;
-// actual vector representing the camera's direction
 float lx = 0.0f, lz = -1.0f;
-// XZ position of the camera
 float x = 0.0f, z = 7.0f;
 
 float deltaAngle = 0.0f;
@@ -30,15 +27,22 @@ float flightLength = 14.0f;
 bool isCrashed = false;
 float wheel1Speed = 0;
 std::vector< glm::vec3 > vertices;
+std::vector< glm::vec3 > vertices_norm;
 std::vector< glm::vec3 > wheel1;
+std::vector< glm::vec3 > wheel1_norm;
 std::vector< glm::vec3 > wheel2;
+std::vector< glm::vec3 > wheel2_norm;
 std::vector< glm::vec3 > tramploine;
+std::vector< glm::vec3 > tramploine_norm;
 
-std::vector< glm::vec3 > loadObj(string path)
+std::vector< glm::vec3 > loadObj(string path, std::vector< glm::vec3 > *result_norm_vertices)
 {
-	std::vector< unsigned int > vertexIndices;//, uvIndices, normalIndices;
+	std::vector< unsigned int > vertexIndices;
+	std::vector< unsigned int > normalIndices;
 	std::vector< glm::vec3 > temp_vertices;
+	std::vector< glm::vec3 > temp_normal_vertices;
 	std::vector< glm::vec3 > result_vertices;
+	/*std::vector< glm::vec3 > result_norm_vertices;*/
 	string word;
 
 	ifstream myfile(path);
@@ -53,24 +57,57 @@ std::vector< glm::vec3 > loadObj(string path)
 				myfile >> vertex.z;
 				temp_vertices.push_back(vertex);
 			}
-			
+			if (word == "vn") {
+				glm::vec3 vertex;
+				myfile >> vertex.x;
+				myfile >> vertex.y;
+				myfile >> vertex.z;
+				temp_normal_vertices.push_back(vertex);
+			}
 			else if (word == "f"){
 				std::string vertex1, vertex2, vertex3;
 				unsigned int vertexIndex[3];
+				unsigned int normalIndex[3];
+				string dot;
+				string normal;
+				int pos = 0;
 				std::string delimiter = "//";
 				myfile >> word;
-				vertexIndex[0] = atoll(word.c_str());
+				pos = word.find(delimiter);
+				dot = word.substr(0, pos);
+				normal = word.substr(pos + delimiter.length(), word.length());	
+				vertexIndex[0] = atoll(dot.c_str());
+				normalIndex[0] = atoll(normal.c_str());
+
 				myfile >> word;
-				vertexIndex[1] = atoll(word.c_str());
+				pos = word.find(delimiter);
+				dot = word.substr(0, pos);
+				normal = word.substr(pos + delimiter.length(), word.length());
+				vertexIndex[1] = atoll(dot.c_str());
+				normalIndex[1] = atoll(normal.c_str());
+
 				myfile >> word;
-				vertexIndex[2] = atoll(word.c_str());
+				pos = word.find(delimiter);
+				dot = word.substr(0, pos);
+				normal = word.substr(pos + delimiter.length(), word.length());
+				vertexIndex[2] = atoll(dot.c_str());
+				normalIndex[2] = atoll(normal.c_str());
 
 				vertexIndices.push_back(vertexIndex[0]);
 				vertexIndices.push_back(vertexIndex[1]);
-				vertexIndices.push_back(vertexIndex[2]);				
+				vertexIndices.push_back(vertexIndex[2]);
+				normalIndices.push_back(normalIndex[0]);
+				normalIndices.push_back(normalIndex[1]);
+				normalIndices.push_back(normalIndex[2]);
 			}
 		}
 		myfile.close();
+	}
+
+	for (unsigned int i = 0; i < normalIndices.size(); i++){
+		unsigned int normalIndex = normalIndices[i];
+		glm::vec3 normal = temp_normal_vertices[normalIndex - 1];
+		result_norm_vertices->push_back(normal);
 	}
 
 	for (unsigned int i = 0; i < vertexIndices.size(); i++){
@@ -101,9 +138,10 @@ void DrawCoach() {
 		glPushMatrix();
 			glRotatef(coachAngle, 0, 0, 1);
 			glBegin(GL_TRIANGLES);	
-			for (int i = 0; i < vertices.size(); i++){
+			for (int i = 0; i < vertices.size(); i++){				
+				glNormal3f(vertices_norm[i].x, vertices_norm[i].y, vertices_norm[i].z);
 				glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
-			}
+			}			
 			glEnd();
 		glPopMatrix();	
 	glPopMatrix();	
@@ -114,6 +152,7 @@ void DrawWheels()
 	glPushMatrix();
 		glTranslatef(coachSpan - coachPos, coachJumpHeight, rangeFromPlayer);
 		glRotatef(coachAngle, 0, 0, 1);
+
 		//big wheel 1
 		glPushMatrix();		
 		glTranslatef(2.0 + wheel1Speed / 1.5, 0.89f + wheel1Speed/ 4, -0.55f + wheel1Speed);
@@ -121,6 +160,7 @@ void DrawWheels()
 				glRotatef(wheelAngle, 0, 0, 1);
 				glBegin(GL_TRIANGLES);
 					for (int i = 0; i < wheel1.size(); i++){
+						glNormal3f(wheel1_norm[i].x, wheel1_norm[i].y, wheel1_norm[i].z);
 						glVertex3f(wheel1[i].x, wheel1[i].y, wheel1[i].z);
 					}
 				glEnd();
@@ -134,6 +174,7 @@ void DrawWheels()
 				glRotatef(wheelAngle, 0, 0, 1);
 				glBegin(GL_TRIANGLES);
 					for (int i = 0; i < wheel1.size(); i++){
+						glNormal3f(wheel1_norm[i].x, wheel1_norm[i].y, wheel1_norm[i].z);
 						glVertex3f(wheel1[i].x, wheel1[i].y, wheel1[i].z);
 					}
 				glEnd();
@@ -147,6 +188,7 @@ void DrawWheels()
 				glRotatef(wheelAngle, 0, 0, 1);
 				glBegin(GL_TRIANGLES);
 					for (int i = 0; i < wheel2.size(); i++){
+						glNormal3f(wheel2_norm[i].x, wheel2_norm[i].y, wheel2_norm[i].z);
 						glVertex3f(wheel2[i].x, wheel2[i].y, wheel2[i].z);
 					}
 				glEnd();
@@ -160,6 +202,7 @@ void DrawWheels()
 				glRotatef(wheelAngle, 0, 0, 1);
 				glBegin(GL_TRIANGLES);
 					for (int i = 0; i < wheel2.size(); i++){
+						glNormal3f(wheel2_norm[i].x, wheel2_norm[i].y, wheel2_norm[i].z);
 						glVertex3f(wheel2[i].x, wheel2[i].y, wheel2[i].z);
 					}
 				glEnd();
@@ -175,9 +218,9 @@ void DrawTrampoline()
 	glColor3f(1.0f, 0.5f, 0.3f);
 	glTranslatef(trampolinePos, 0, rangeFromPlayer + -1.5);
 	glRotatef(-90, 0, 1, 0);
-
 	glBegin(GL_TRIANGLES);
-	for (int i = 0; i < tramploine.size(); i++){
+	for (int i = 0; i < tramploine.size(); i++){		
+		glNormal3f(tramploine_norm[i].x, tramploine_norm[i].y, tramploine_norm[i].z);
 		glVertex3f(tramploine[i].x, tramploine[i].y, tramploine[i].z);
 	}
 	glEnd();
@@ -200,19 +243,23 @@ void RenderScene(void)
 	GLfloat color[] = { 0.384f, 0.776f, 0.533f, 1.f };
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, color);
 	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);
 	glVertex3f(-100.0f, 0.0f, -100.0f);
+	glNormal3f(0, 1, 0);
 	glVertex3f(-100.0f, 0.0f, 100.0f);
+	glNormal3f(0, 1, 0);
 	glVertex3f(100.0f, 0.0f, 100.0f);
+	glNormal3f(0, 1, 0);
 	glVertex3f(100.0f, 0.0f, -100.0f);
 	glEnd();
 
-	GLfloat color2[] = { 1.f, 0.8f, 1.f, 0.2f };
+	GLfloat color2[] = { 0.862, 0.803, 0.156, 0.2f };
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, color2);
 	DrawCoach();
 
-	GLfloat color3[] = { 0.862, 0.803, 0.156, 0.2f };
+	/*GLfloat color3[] = { 0.862, 0.803, 0.156, 0.2f };
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, color3);
-	DrawWheels();
+	DrawWheels();*/
 
 	GLfloat color4[] = { 0.6, 0.301, 0.058, 0.2f };
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, color4);
@@ -254,7 +301,7 @@ void TimerFunc(int value)
 	}
 
 	glutPostRedisplay();
-	glutTimerFunc(14, TimerFunc, 1);
+	glutTimerFunc(3, TimerFunc, 1);
 }
 
 void changeSize(int w, int h) {
@@ -273,7 +320,7 @@ void changeSize(int w, int h) {
 	glViewport(0, 0, w, h);
 
 	// Set the correct perspective.
-	gluPerspective(45.0f, ratio, 0.1f, 50.0f);
+	gluPerspective(45.0f, ratio, 0.5f, 50.0f);
 
 	// Get Back to the Modelview
 	glMatrixMode(GL_MODELVIEW);
@@ -316,10 +363,11 @@ int main(int argc, char* argv[])
 	glutIdleFunc(RenderScene);
 	glutTimerFunc(14, TimerFunc, 1);
 
-	vertices = loadObj("D:\\coach5.obj");
-	wheel1 = loadObj("D:\\wheel4.obj");
-	wheel2 = loadObj("D:\\wheel3.obj");
-	tramploine = loadObj("D:\\tramploine.obj");
+	vertices = loadObj("D:\\coachNorm.obj",  &vertices_norm);
+	wheel1 = loadObj("D:\\wheel4_norm.obj", &wheel1_norm);
+
+	wheel2 = loadObj("D:\\wheel3_norm.obj", &wheel2_norm);
+	tramploine = loadObj("D:\\untitled.obj", &tramploine_norm);
 
 	glutSpecialFunc(PressKey);
 	glutSpecialUpFunc(ReleaseKey);
@@ -329,9 +377,10 @@ int main(int argc, char* argv[])
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-	GLfloat lightpos[] = { -7, 0.0, 10, 0.2};
+	//glEnable(GL_NORMALIZE);
+	GLfloat lightpos[] = { -5, 15, 0, 0.5};
 	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
-
+	//glFrontFace(GL_CCW);
 	glutMainLoop();
 
 	return 0;
